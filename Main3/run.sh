69df9f8aa9e2616d3cf5aa0f6cf1ac867695397b
Vasilii Rogin <vasilii@giosg.com>
2020-05-22 10:41:18 +0300

    Add jar file build

--- Main3/run.sh
+++ Main3/run.sh
@@ -3,9 +3,18 @@ set -e
 echo "Clean"
 rm -Rf out || true
 
-echo "Build"
+echo "Compile"
 javac src/ru/novsu/povt/roginvs/graphics/*.java src/ru/novsu/povt/roginvs/*.java -d out
+
+echo "Build package"
+(
+    cd out &&
+    jar -cfve my.jar \
+      ru/novsu/povt/roginvs/Roginvs \
+      ru/novsu/povt/roginvs/*.class \
+      ru/novsu/povt/roginvs/graphics/*.class 
+)
 
 echo "Run"
 echo ""
-(cd out && java ru.novsu.povt.roginvs.Roginvs)
+(cd out && java -jar my.jar)
