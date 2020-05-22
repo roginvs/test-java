a54e05430e12b3d383fdf557d3bc29fdf680fa59
Vasilii Rogin <vasilii@giosg.com>
2020-05-22 12:54:10 +0300

    Add serialization

--- Main6/Main6.java
+++ Main6/Main6.java
@@ -1,9 +1,13 @@
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Main6 {
     private static String SIN_FILENAME = "sin.txt";
@@ -44,10 +48,79 @@ public class Main6 {
         System.out.println(String.format("Readed line %d = %f", lineNum, value));
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    private static void serializeObjects() throws IOException, ClassNotFoundException {
+        System.out.println("Serializing array");
+
+        var SERIALIZE_FILE_NAME = "sin2.dat";
+
+        double[] values = new double[360];
+        for (int degree = 0; degree < 360; degree++) {
+            double rad = degree * Math.PI / 180;
+            values[degree] = Math.sin(rad);
+        }
+
+        try (var oos = new ObjectOutputStream(new FileOutputStream(SERIALIZE_FILE_NAME))) {
+            oos.writeObject(values);
+        }
+        
+
+        System.out.println("Deserializing array");
+
+        double[] valuesReaded = new double[360];
+        try (var oos = new ObjectInputStream(new FileInputStream(SERIALIZE_FILE_NAME))) {
+            valuesReaded = (double[]) oos.readObject();
+        }
+        
+
+        System.out.println("Serializing one-by-one");
+
+        var SERIALIZE_ONE_BY_ONE_FILE_NAME = "sin2-one-by-one.dat";
+        try (var oos = new ObjectOutputStream(new FileOutputStream(SERIALIZE_ONE_BY_ONE_FILE_NAME))) {
+            for (var value : values) {
+                oos.writeObject(value);
+            }
+        }
+        
+        System.out.println("Deserializing one-by-one");
+
+        double[] valuesOneByOneReaded = new double[360];
+        try (var oos = new ObjectInputStream(new FileInputStream(SERIALIZE_ONE_BY_ONE_FILE_NAME))) {
+            for (int i = 0; i < 360; i++) {
+                valuesOneByOneReaded[i] = (double) oos.readObject();
+            }
+        }
+        
+        System.out.println("Checking");
+
+        if (valuesReaded.length != values.length) {
+            throw new Error("Not equal length for array serialization");
+        }
+        
+
+        if (valuesOneByOneReaded.length != values.length) {
+            throw new Error("Not equal length for one-by-one");
+        }
+        
+
+        for (int i = 0; i < 360; i++) {
+            if (values[i] != valuesReaded[i]) {
+                throw new Error(String.format("Wrong value at pos %d for object serialization", i));
+            }
+            if (values[i] != valuesOneByOneReaded[i]) {
+                throw new Error(String.format("Wrong value at pos %d for one-by-one serialization", i));
+            }
+        }
+
+        System.out.println("All equal");
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         Main6.createSinFile();
         Main6.createInputFile();
 
         Main6.readFiles();
+
+        Main6.serializeObjects();
     }
 }
