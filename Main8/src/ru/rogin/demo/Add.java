38afc605365bba79f462e0c268f6783ac544374e
Vasilii Rogin <vasilii@giosg.com>
2020-05-23 13:53:03 +0300

    Add servlet

--- Main8/src/ru/rogin/demo/Add.java
+++ Main8/src/ru/rogin/demo/Add.java
@@ -5,5 +5,12 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Add extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        response.setContentType("text/html");
     
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "lol kek" + "</h1>");
+    }
 }
