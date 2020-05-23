519cb293d3f7fbc8324b63a5f211c4b792375e4c
Vasilii Rogin <vasilii@giosg.com>
2020-05-23 15:44:21 +0300

    Add mysql connector

--- Main8/src/ru/rogin/demo/Add.java
+++ Main8/src/ru/rogin/demo/Add.java
@@ -3,14 +3,33 @@ package ru.rogin.demo;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Add extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
         response.setContentType("text/html");
-    
+
         PrintWriter out = response.getWriter();
-        out.println("<h1>" + "lol kek" + "</h1>");
+
+        try {
+            String url = "jdbc:mysql://localhost/productdb?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+
+            String password = "password";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)) {
+
+                out.println("Connection to ProductDB succesfull!");
+            }
+
+        } catch (Exception ex) {
+            out.println("Connection failed...");
+            out.println(ex);
+        } finally {
+            out.close();
+        }
+
     }
 }
